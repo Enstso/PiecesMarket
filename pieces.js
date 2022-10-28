2657edf8394e13ea8b474e6d04825114de9a7c56
Enstso <enstso@outlook.fr>
2022-10-28 18:41:28 +0200

    Ajout listes pieces  abordables et disponibles

--- pieces.js
+++ pieces.js
@@ -7,6 +7,7 @@ for (let i=0;i<piece.length;i++){
     const fiches = document.querySelector('.fiches');
 
     const elmt = piece[i];
+    
 
     const imageElement = document.createElement('img');
     imageElement.src = elmt.image;
@@ -75,3 +76,30 @@ boutonTrierDecroissant.addEventListener('click',function(){
     });
     console.log(listPieces2);
 })
+
+const elementsAbordables = document.createElement('ul');
+
+
+for(let i = 0;i< piece.length;i++){
+    if(piece[i].prix<35){
+        const nomElement1 = document.createElement('li');
+        nomElement1.innerText = piece[i].nom;
+        elementsAbordables.appendChild(nomElement1);
+    }
+}
+
+document.querySelector('.abordables').appendChild(elementsAbordables);
+
+const elementsDisonibles = document.createElement('ul');
+
+
+for(let i=0;i<piece.length;i++){
+    if(piece[i].disponibilite == true){
+        const nomElement2 = document.createElement('li');
+        nomElement2.innerText = piece[i].nom + " - "+ piece[i].prix + " $";
+        elementsDisonibles.appendChild(nomElement2);
+    }
+}
+
+
+document.querySelector('.disponibles').appendChild(elementsDisonibles);
